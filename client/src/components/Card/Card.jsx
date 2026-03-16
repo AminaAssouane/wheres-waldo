65dd39f5922a1f36ad993a2cf690f7492bcd7bdd
Amina <minassouane@gmail.com>
2026-03-16 08:39:21 +0100

    feat(game): add card to preview game

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,4 +1,10 @@
 import styles from "./Card.module.css";
-export function Card() {
-  return <article className={styles.card}></article>;
+export function Card({ imageUrl, title }) {
+  return (
+    <article className={styles.card}>
+      <img src={imageUrl} alt="" />
+      <div className={styles.title}>{title}</div>
+      <button className={styles.startBtn}>Start Game</button>
+    </article>
+  );
 }
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
 import styles from "./Home.module.css";
+import { Card } from "../../components/Card/Card.jsx";
 export function Home() {
   return (
     <section className={styles.home}>
       <h1 className={styles.title}>Games</h1>
+      <Card
+        imageUrl="/public/images/universe.jpeg"
+        title="Universe Infested"
+      ></Card>
     </section>
   );
 }
