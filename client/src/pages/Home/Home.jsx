1b6e30d23118a1c3859593925801a7cd993fab80
Amina <minassouane@gmail.com>
2026-03-18 23:40:05 +0100

    fix(images): change urls to accomodate netlify

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,10 +4,7 @@ export function Home() {
   return (
     <section className={styles.home}>
       <h1 className={styles.title}>Game</h1>
-      <Card
-        imageUrl="/public/images/universe.jpeg"
-        title="Universe Infested"
-      ></Card>
+      <Card imageUrl="/images/universe.jpeg" title="Universe Infested"></Card>
     </section>
   );
 }
